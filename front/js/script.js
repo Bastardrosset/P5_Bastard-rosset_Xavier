1f87d45efc72e4860cb7b9cf30971a18af1a0256
Bastardrosset <87719767+Bastardrosset@users.noreply.github.com>
2022-06-09 18:56:22 +0200

    Modification front/script.js fonction displayArticle page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,6 @@
  async function main(){
      const articles = await getArticles()
-     console.log(articles)
+    //  console.log(articles)
      displayArticles(articles)
  }
 
@@ -11,17 +11,34 @@
         return httpListArticles.json()
     })
     .then(function(articles){
-        console.log(articles)
+        // console.log(articles)
         return articles
     })
     .catch(function(error){
         alert(error)
     })
  }
-// 
+ main()
+// Affichage des articles page d'accueil //
+
  function displayArticles(articles){
     for(let article of articles){
+        let lienArticle = document.createElement('a');
+        lienArticle.href += `./product.html?id=${article._id}`;
+        let contentArticle = document.createElement('article');
+
+        let imgArticle = document.createElement('img');
+        imgArticle.src = `${article.imageUrl}`
+
+        let titleArticle = document.createElement('h3');
+        titleArticle.textContent = `${article.name}`
+
+        let descArticle = document.createElement('p');
+        descArticle.append =`${article.description}`;
+
+        document.getElementById('items').appendChild(lienArticle);
+        lienArticle.appendChild(contentArticle);
+        contentArticle.appendChild(imgArticle,titleArticle,descArticle)
         console.log(article);
     }
  }
- main()
