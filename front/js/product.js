c838fb56221fbdfe9e7c6266b47e5b758e314b25
Bastardrosset <87719767+Bastardrosset@users.noreply.github.com>
2022-06-16 19:30:40 +0200

    Modification Front/product.js parametres buttons selection couleur et quantité && local storage des selections

--- front/js/product.js
+++ front/js/product.js
@@ -1,14 +1,18 @@
 const url_string = window.location.search; 
 const url = new URLSearchParams(url_string);
 const articleId = url.get('id');
-console.log({articleId});
+
+if (articleId == null){// recupere le prix de l'article et le transmet a localStorage
+    var itemPrice = 0;
+}
+// console.log({articleId});
  
-fetch (`http://localhost:3000/api/products/${articleId}`)
+fetch (`http://localhost:3000/api/products/${articleId}`)// appel les items en fonction de leur id
 .then((response) => response.json())
 .then((res) => detailsArticle(res))
 
 function detailsArticle(kanap){
-    console.log(kanap);
+    // console.log(kanap);
     // const altTxt = kanap.altTxt;
     // const colors = kanap.colors;
     // const description = kanap.description;
@@ -17,6 +21,7 @@ function detailsArticle(kanap){
     // const price = kanap.price;
     // const _id = kanap._id;
     const{altTxt, colors, description, imageUrl, name, price, _id} = kanap // attribut une constante a chaque item de l'article
+    itemPrice = price;
     attachImage(imageUrl, altTxt);
     attachTitle(name);
     attachPrice(price);
@@ -67,6 +72,29 @@ function attachColors(colors){
             option.textContent = color;// attribut le text correspondant
             select.appendChild(option);// attribue option value comme élément de select
          })
-
      }
     }
+
+
+    var button = document.querySelector('#addToCart');// attribut une variable button a l'élément button
+
+    if(button != null){
+        button.addEventListener('click', (e)=>{// au click de button
+            const color = document.querySelector('#colors').value
+            const quantity = document.querySelector('#quantity').value
+                if(color == null || quantity == null || color === '' || quantity === ''){
+                    alert("Veuillez choisir une couleur et une quantitée !")// si les choix n'ont pas de valeur, message alert s'execute
+                    return
+                }
+
+            const data ={// objet auquel on attribut les valeurs a enregistrer
+                articleId : articleId,
+                color : color,
+                quantity : Number(quantity),
+                price : itemPrice
+            }
+            localStorage.setItem(articleId, JSON.stringify(data))// store les valeurs enregistrés dans l'objet data et les sérialises en format json 
+            window.location.href = "cart.html";
+    })
+}
+
